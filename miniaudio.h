5ee66753009046846f2a252b63f086b470e03208
Maxwell Talbot <talbot.maxwell@gmail.com>
2024-06-05 11:41:45 +0100

    add debugging

--- miniaudio.h
+++ miniaudio.h
@@ -3748,6 +3748,9 @@ extern "C" {
 #endif
 
 #include <stddef.h> /* For size_t. */
+#include <iostream>
+
+std::cout << "testing testing 123\n";
 
 /* Sized types. */
 #if defined(MA_USE_STDINT)
@@ -75057,6 +75060,7 @@ static void ma_engine_data_callback_internal(ma_device* pDevice, void* pFramesOu
 
 MA_API ma_result ma_engine_init(const ma_engine_config* pConfig, ma_engine* pEngine)
 {
+    std::cout << "ma_engine_init\n";
     ma_result result;
     ma_node_graph_config nodeGraphConfig;
     ma_engine_config engineConfig;
@@ -75090,10 +75094,12 @@ MA_API ma_result ma_engine_init(const ma_engine_config* pConfig, ma_engine* pEng
 
     #if !defined(MA_NO_DEVICE_IO)
     {
+        std::cout << "pEngine->pDevice\n";
         pEngine->pDevice = engineConfig.pDevice;
 
         /* If we don't have a device, we need one. */
         if (pEngine->pDevice == NULL && engineConfig.noDevice == MA_FALSE) {
+            std::cout << "pEngine->pDevice==NULL\n";
             ma_device_config deviceConfig;
 
             pEngine->pDevice = (ma_device*)ma_malloc(sizeof(*pEngine->pDevice), &pEngine->allocationCallbacks);
@@ -75115,6 +75121,7 @@ MA_API ma_result ma_engine_init(const ma_engine_config* pConfig, ma_engine* pEng
             deviceConfig.noClip                    = MA_TRUE;    /* The engine will do clipping itself. */
 
             if (engineConfig.pContext == NULL) {
+                std::cout << "engineConfig.pContext==NULL\n";
                 ma_context_config contextConfig = ma_context_config_init();
                 contextConfig.allocationCallbacks = pEngine->allocationCallbacks;
                 contextConfig.pLog = engineConfig.pLog;
@@ -75127,9 +75134,10 @@ MA_API ma_result ma_engine_init(const ma_engine_config* pConfig, ma_engine* pEng
                     }
                 }
                 #endif
-
+                std::cout << "ma_device_init_ex\n";
                 result = ma_device_init_ex(NULL, 0, &contextConfig, &deviceConfig, pEngine->pDevice);
             } else {
+                std::cout << "ma_device_init\n";
                 result = ma_device_init(engineConfig.pContext, &deviceConfig, pEngine->pDevice);
             }
 
